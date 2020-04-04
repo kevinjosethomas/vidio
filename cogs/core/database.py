b2991128a77774093d4b8934b551a8353db01d92
TrustedMercury <kevin.jt2007@gmail.com>
2020-04-04 13:12:33 -0400

    Added add_channel, add_guild, get_channel, get_leaderboard, set_prefix and upload_video methods.

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -12,6 +12,113 @@ class Database(commands.Cog):
         self.bot = bot
         self.db = self.bot.db
 
+        bot.algorithm = {
+            'fail': {
+                'views': {
+                    1: 8,
+                    2: 20,
+                    3: 10,
+                    4: 10,
+                    5: 10,
+                    6: 5,
+                    7: 5,
+                    8: 5,
+                    9: 5,
+                    10: 5,
+                    11: 5
+                },
+                'subscribers': -30,
+                'stats': {
+                    'likes': [1, 3],
+                    'dislikes': [5, 7]
+                }
+            },
+
+            'poor': {
+                'views': {
+                    1: 2,
+                    2: 30,
+                    3: 20,
+                    4: 20,
+                    5: 20,
+                    6: 10,
+                    7: 5,
+                    8: 5,
+                    9: 5,
+                    10: 5,
+                    11: 5
+                },
+                'subscribers': [-10, 10],
+                'stats': {
+                    'likes': [2, 4],
+                    'dislikes': [2, 4]
+                }
+            },
+
+            'average': {
+                'views': {
+                    1: 15,
+                    2: 30,
+                    3: 20,
+                    4: 20,
+                    5: 20,
+                    6: 10,
+                    7: 5,
+                    8: 5,
+                    9: 5,
+                    10: 5,
+                    11: 5
+                },
+                'subscribers': 15,
+                'stats': {
+                    'likes': [3, 5],
+                    'dislikes': [1, 3]
+                }
+            },
+
+            'good': {
+                'views': {
+                    1: 20,
+                    2: 40,
+                    3: 30,
+                    4: 20,
+                    5: 10,
+                    6: 10,
+                    7: 5,
+                    8: 5,
+                    9: 5,
+                    10: 5,
+                    11: 5
+                },
+                'subscribers': 20,
+                'stats': {
+                    'likes': [8, 10],
+                    'dislikes': [1, 3]
+                }
+            },
+
+            'trending': {
+                'views': {
+                    1: 10000,
+                    2: 10,
+                    3: 10,
+                    4: 8,
+                    5: 8,
+                    6: 5,
+                    7: 3,
+                    8: 3,
+                    9: 3,
+                    10: 3,
+                    11: 3
+                },
+                'subscribers': 10,
+                'stats': {
+                    'likes': [8, 10],
+                    'dislikes': [1, 3]
+                }
+            }
+        }
+
     @staticmethod
     async def text_check(text):
 
@@ -50,19 +157,53 @@ class Database(commands.Cog):
         else:
             raise Exception(TypeError(f'Expected str or list, received {type(text)}'))
 
+    async def add_channel(self, user_id, name, description, category):
+
+        if not self.text_check([name, description, category]):
+            return 'Bad Arguments'
+
+        async with self.db.acquire() as conn:
+            user = self.bot.db.fetch(
+                "SELECT * FROM users WHERE user_id = %s",
+                (user_id,))
+            if not user:
+                await conn.execute(
+                    "INSERT INTO users (user_id, money) VALUES (%s, %s)",
+                    (user_id, 0))
+
+            channels = await self.get_channel(user_id)
+            for channel in channels:
+                if channel[2].lower() == name.lower():
+                    return 'Channel with same name exists'
+
+            await conn.execute(
+                "INSERT INTO channels (user_id, name, description, subscribers, "
+                "total_views, category, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                (user_id, name, description, 0, 0, category, datetime.today()))
+
+        return 'Successful'
+
+    async def add_guild(self, guild):
+
+        async with self.db.acquire() as conn:
+            await conn.execute(
+                "INSERT INTO guilds (guild_id, prefix) VALUES (%s, %s)",
+                (guild.id, '-'))
+
     async def get_channel(self, query_id):
 
         if isinstance(query_id, int):
             if len(str(query_id)) >= 15:  # Provided id is a a discord snowflake.
-                channel = await self.db.fetchrow(
+                channel = await self.db.fetch(
                     "SELECT * FROM channels WHERE user_id = %s",
                     (query_id,))
                 if channel:
                     return channel
                 else:
-                    return 'Channel doesn\'t exist'
+                    return "Channel doesn't exist"
+
             else:  # Provided id is a channel id.
-                channel = await self.db.fetchrow(
+                channel = await self.db.fetch(
                     "SELECT * FROM channels WHERE channel_id = %s",
                     (query_id,))
                 if channel:
@@ -72,26 +213,172 @@ class Database(commands.Cog):
         else:
             raise TypeError(f'Expected int, received {type(query_id)}')
 
-    async def add_channel(self, user_id, name, description, category):
+    async def get_leaderboard(self):
 
-        if not self.text_check([name, description, category]):
-            return 'Bad Arguments'
+        leaderboard = await self.db.fetch(
+            "SELECT user_id, name, subscribers FROM channels "
+            "ORDER BY subscribers DESC LIMIT 10")
+
+        return leaderboard
+
+    async def set_prefix(self, guild, prefix):
 
         async with self.db.acquire() as conn:
-            # This was user = self.bot.db(... Not sure if it needs to be fetchrow or fetch /shrug
-            user = self.bot.db.fetchrow(
-                "SELECT * FROM users WHERE user_id = %s",
-                (user_id,))
-            if not user:
-                await conn.execute(
-                    "INSERT INTO users (user_id, money) VALUES (%s, %s)",
-                    (user_id, 0))
-            await conn.execute(
-                "INSERT INTO channels (user_id,  name, description, subscribers, "
-                "total_views, category, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s",
-                (user_id, name, description, 0, 0, category, datetime.today()))
+            conn.execute(
+                "UPDATE guilds SET prefix = %s WHERE guild_id = %s",
+                (prefix, guild.id))
 
-        return 'Successful'
+    async def upload_video(self, user_id, channel, name, description):
+
+        if not self.text_check([name, description]):
+            return 'Bad Arguments'
+
+        choices = ['fail', 'poor', 'average', 'good', 'trending']
+        status = random.choices(choices, weights=[20, 20, 50, 9.9999, 0.0001])
+
+        channel_data = self.db.fetch(
+            "SELECT channel_id, subscribers, total_views FROM channels WHERE user_id = %s AND name = %s",
+            (user_id, channel))
+        channel_id = channel_data[0]
+        subscribers = channel_data[1]
+        total_views = channel_data[2]
+
+        if status == 'fail':
+            last_percentage = 1
+            views = self.bot.algorithm['fail']['views'][last_percentage] * subscribers / 100
+            new_subscribers = self.bot.algorithm['fail']['subscribers'] * views / 100
+            likes = random.randint(
+                self.bot.algorithm['fail']['stats']['likes'][0],
+                self.bot.algorithm['fail']['stats']['likes'][1]) * views / 100
+            dislikes = random.randint(
+                self.bot.algorithm['fail']['stats']['dislikes'][0],
+                self.bot.algorithm['fail']['stats']['dislikes'][1]
+            ) * views / 100
+            subscribers += new_subscribers
+            total_views += views
+
+            async with self.db.acquire() as conn:
+                conn.execute(
+                    "INSERT INTO videos (channel_id, name, description, status, new_subs, views, "
+                    "likes, dislikes, last_percentage, last_updated, created_at) VALUES (%s, %s, %s"
+                    "%s, %s, %s, %s, %s, %s, %s, %s)",
+                    (channel_id, name, description, status, new_subscribers, views, likes, dislikes,
+                     last_percentage, datetime.now(), datetime.today()))
+
+                conn.execute(
+                    "UPDATE channels SET subscribers = %s, total_views = %s WHERE channel_id = %s",
+                    (subscribers, total_views, channel_id))
+
+        elif status == 'poor':
+            last_percentage = 1
+            views = self.bot.algorithm['poor']['views'][last_percentage] * subscribers / 100
+            new_subscribers = self.bot.algorithm['poor']['subscribers'] * views / 100
+            likes = random.randint(
+                self.bot.algorithm['poor']['stats']['likes'][0],
+                self.bot.algorithm['poor']['stats']['likes'][1]) * views / 100
+            dislikes = random.randint(
+                self.bot.algorithm['poor']['stats']['dislikes'][0],
+                self.bot.algorithm['poor']['stats']['dislikes'][1]
+            ) * views / 100
+            subscribers += new_subscribers
+            total_views += views
+
+            async with self.db.acquire() as conn:
+                conn.execute(
+                    "INSERT INTO videos (channel_id, name, description, status, new_subs, views, "
+                    "likes, dislikes, last_percentage, last_updated, created_at) VALUES (%s, %s, %s"
+                    "%s, %s, %s, %s, %s, %s, %s, %s)",
+                    (channel_id, name, description, status, new_subscribers, views, likes, dislikes,
+                     last_percentage, datetime.now(), datetime.today()))
+
+                conn.execute(
+                    "UPDATE channels SET subscribers = %s, total_views = %s WHERE channel_id = %s",
+                    (subscribers, total_views, channel_id))
+
+        elif status == 'average':
+            last_percentage = 1
+            views = self.bot.algorithm['average']['views'][last_percentage] * subscribers / 100
+            new_subscribers = self.bot.algorithm['average']['subscribers'] * views / 100
+            likes = random.randint(
+                self.bot.algorithm['average']['stats']['likes'][0],
+                self.bot.algorithm['average']['stats']['likes'][1]) * views / 100
+            dislikes = random.randint(
+                self.bot.algorithm['average']['stats']['dislikes'][0],
+                self.bot.algorithm['average']['stats']['dislikes'][1]
+            ) * views / 100
+            subscribers += new_subscribers
+            total_views += views
+
+            async with self.db.acquire() as conn:
+                conn.execute(
+                    "INSERT INTO videos (channel_id, name, description, status, new_subs, views, "
+                    "likes, dislikes, last_percentage, last_updated, created_at) VALUES (%s, %s, %s"
+                    "%s, %s, %s, %s, %s, %s, %s, %s)",
+                    (channel_id, name, description, status, new_subscribers, views, likes, dislikes,
+                     last_percentage, datetime.now(), datetime.today()))
+
+                conn.execute(
+                    "UPDATE channels SET subscribers = %s, total_views = %s WHERE channel_id = %s",
+                    (subscribers, total_views, channel_id))
+
+        elif status == 'good':
+            last_percentage = 1
+            views = self.bot.algorithm['good']['views'][last_percentage] * subscribers / 100
+            new_subscribers = self.bot.algorithm['good']['subscribers'] * views / 100
+            likes = random.randint(
+                self.bot.algorithm['good']['stats']['likes'][0],
+                self.bot.algorithm['good']['stats']['likes'][1]) * views / 100
+            dislikes = random.randint(
+                self.bot.algorithm['good']['stats']['dislikes'][0],
+                self.bot.algorithm['good']['stats']['dislikes'][1]
+            ) * views / 100
+            subscribers += new_subscribers
+            total_views += views
+
+            async with self.db.acquire() as conn:
+                conn.execute(
+                    "INSERT INTO videos (channel_id, name, description, status, new_subs, views, "
+                    "likes, dislikes, last_percentage, last_updated, created_at) VALUES (%s, %s, %s"
+                    "%s, %s, %s, %s, %s, %s, %s, %s)",
+                    (channel_id, name, description, status, new_subscribers, views, likes, dislikes,
+                     last_percentage, datetime.now(), datetime.today()))
+
+                conn.execute(
+                    "UPDATE channels SET subscribers = %s, total_views = %s WHERE channel_id = %s",
+                    (subscribers, total_views, channel_id))
+
+        elif status == 'trending':
+            last_percentage = 1
+            views = self.bot.algorithm['trending']['views'][last_percentage] * subscribers / 100
+            new_subscribers = self.bot.algorithm['trending']['subscribers'] * views / 100
+            likes = random.randint(
+                self.bot.algorithm['trending']['stats']['likes'][0],
+                self.bot.algorithm['trending']['stats']['likes'][1]) * views / 100
+            dislikes = random.randint(
+                self.bot.algorithm['trending']['stats']['dislikes'][0],
+                self.bot.algorithm['trending']['stats']['dislikes'][1]
+            ) * views / 100
+            subscribers += new_subscribers
+            total_views += views
+
+            async with self.db.acquire() as conn:
+                conn.execute(
+                    "INSERT INTO videos (channel_id, name, description, status, new_subs, views, "
+                    "likes, dislikes, last_percentage, last_updated, created_at) VALUES (%s, %s, %s"
+                    "%s, %s, %s, %s, %s, %s, %s, %s)",
+                    (channel_id, name, description, status, new_subscribers, views, likes, dislikes,
+                     last_percentage, datetime.now(), datetime.today()))
+
+                conn.execute(
+                    "UPDATE channels SET subscribers = %s, total_views = %s WHERE channel_id = %s",
+                    (subscribers, total_views, channel_id))
+
+        return {'status': status,
+                'new_subs': new_subscribers,
+                'views': views,
+                'likes': likes,
+                'dislikes': dislikes}
+
 
 def setup(bot):
     bot.add_cog(Database(bot))
