b4c8052c6e9f25c234fd5679cc67ec1bf3c35b57
TrustedMercury <kevin.jt2007@gmail.com>
2020-04-16 19:09:28 -0400

    Major Resource Extensive Bugs, that were fixed

--- cogs/core/settings.py
+++ cogs/core/settings.py
@@ -250,7 +250,7 @@ class Default(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
 
-        if self.bot.user.mentioned_in(message.clean_content):
+        if self.bot.user in message.mentions:
 
             await message.channel.send(f'**The prefix for this server is -'
                                        f'** ``{(await self.database.get_prefix(message.guild))[0]}``')
