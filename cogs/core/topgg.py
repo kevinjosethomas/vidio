8b5270d885c3677dbe39eaebee0e163990c439b6
TrustedMercury <kevin.jt2007@gmail.com>
2020-04-19 14:23:03 -0400

    added to test vote

--- cogs/core/topgg.py
+++ cogs/core/topgg.py
@@ -50,6 +50,27 @@ class TopGG(commands.Cog):
         print("Successful Test")
         print(data)
 
+        bot_id = int(data["bot"])
+        user_id = int(data["user"])
+        upvote_type = data["type"]
+        is_weekend = data["isWeekend"]
+
+        if bot_id != self.bot.user.id:
+            return
+
+        upvote = self.database.on_vote(user_id, is_weekend)
+
+        if upvote == 'User doesn\'t exist':
+            return
+
+        money = upvote[0]
+        added_money = upvote[1]
+
+        user = self.bot.get_user(user_id)
+
+        await user.send(f':heart: **Thanks for upvoting!** {self.bot.money} You got ${added_money} for helping '
+                        f'videonet grow!')
+
 
 def setup(bot):
     bot.add_cog(TopGG(bot))
