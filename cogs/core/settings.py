2097f13cf67aa9d0fcffa7302c8918e78689c493
TrustedMercury <kevin.jt2007@gmail.com>
2020-04-21 13:58:04 -0400

    Debugging

--- cogs/core/settings.py
+++ cogs/core/settings.py
@@ -17,11 +17,10 @@ class MyHelpCommand(commands.HelpCommand):
             command_list = list(map(lambda c: c.name, set(mapping[category])))
 
             for command in command_list:
-                print(command)
-                print(command_list)
                 command_index = command_list.index(command)
-                print(command_index)
                 command_list[command_index] = f'``{command}``'
+
+            print(command_list)
 
             command = command.join(command_list)
 
