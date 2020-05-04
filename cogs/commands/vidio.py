e2489587405e0c346244315be415a21212c2c203
TrustedMercury <kevin.jt2007@gmail.com>
2020-05-04 16:38:30 -0400

    Minor Bug Fixes

--- cogs/commands/vidio.py
+++ cogs/commands/vidio.py
@@ -232,7 +232,7 @@ class Vidio(commands.Cog):
         award_str = ''
 
         for award in awards:
-            award_str += f"{eval(f'self.bot.{award.value()}')} "
+            award_str += f"{eval(f'self.bot.{award.values()}')} "
 
         user = self.bot.get_user(user)
 
