11dadc34695f7bc82f35ccea71add69be5243652
TrustedMercury <kevin.jt2007@gmail.com>
2020-05-14 10:35:05 -0400

    Major Bug Fixes

--- cogs/commands/vidio.py
+++ cogs/commands/vidio.py
@@ -306,7 +306,7 @@ class Vidio(commands.Cog):
         award_str = ''
 
         for award in awards:
-            award_str += f"{eval(f'self.bot.{str(award[0])}')} "
+            award_str += f"{self.bot.EMOJIS[str(award[0])]} "
 
         user = self.bot.get_user(user)
 
