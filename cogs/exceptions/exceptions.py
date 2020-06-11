842cb7fcd9a7ab0c51ca02508313eb66a9d52db1
TrustedMercury <kevin.jt2007@gmail.com>
2020-06-11 08:48:22 -0400

    added InvalidUser exception

--- cogs/exceptions/exceptions.py
+++ cogs/exceptions/exceptions.py
@@ -6,15 +6,23 @@ file containing all custom errors needed for the bot
 class UnknownError(Exception):
 
     def __init__(self, reason: str = "Unkown reason!"):
-        """basic initializatin of UnknownError exception"""
+        """basic initialization of UnknownError exception"""
 
         self.message = reason
         super().__init__(self.message)
 
     def __str__(self):
-
         return self.message
 
 
+class InvalidUser(Exception):
 
+    def __init__(self):
+        """basic initialization of the InvalidUser exception"""
+
+        self.message = "Invalid user_id was provided"
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
 
