0bc7ae17ef6f6540e633a9383da081b3beaeb867
TrustedMercury <kevin.jt2007@gmail.com>
2020-12-19 22:00:11 -0500

    Added database setup and changed default prefix

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import asyncpg
 import discord
 import classyjson
@@ -8,15 +9,33 @@ from discord.ext import commands
 
 # Credentials
 load_dotenv()
-token = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASS = os.getenv("DATABASE_PASS")
 
 
 # Initialization
 bot = commands.AutoShardedBot(
-    command_prefix="@",
+    command_prefix="yt!",
     case_insentive=True,
     intents=discord.Intents.default()
 )
+
+
+# Database Setup
+async def setup_database():
+    """Created a database pool connection"""
+
+    bot.database = await asyncpg.create_pool(
+        host=DATABASE_HOST,
+        database=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASS
+    )
+
+asyncio.get_event_loop().run_until_complete(setup_database())
 
 
 # Configuration
@@ -36,4 +55,4 @@ bot.cog_list = [
 for cog in bot.cog_list:
     bot.load_extension(cog)
 
-bot.run(token)
+bot.run(TOKEN)
