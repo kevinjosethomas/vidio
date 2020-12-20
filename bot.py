a42051cca41cb5f5da97689c4f415ef004ed703f
TrustedMercury <kevin.jt2007@gmail.com>
2020-12-19 21:43:44 -0500

    Added configuration JSON

--- bot.py
+++ bot.py
@@ -1,18 +1,33 @@
 import os
 import asyncpg
 import discord
+import classyjson
 from dotenv import load_dotenv
 from discord.ext import commands
 
+
+# Credentials
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
 
+
+# Initialization
 bot = commands.AutoShardedBot(
     command_prefix="@",
     case_insentive=True,
     intents=discord.Intents.default()
 )
 
+
+# Configuration
+with open("data/config.json", "r") as _config:
+    bot.c = classyjson.load(_config)
+
+with open("data/emojis.json", "r") as _emojis:
+    bot.e = classyjson.load(_emojis)
+
+
+# Execution
 bot.cog_list = [
     "cogs.core.events"
 ]
