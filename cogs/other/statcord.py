2e117a4a228a0749c5d142f44dd509c6a3943b56
TrustedMercury <kevin.jt2007@gmail.com>
2020-12-22 09:44:51 -0500

    Minor Bug Fixes

--- cogs/other/statcord.py
+++ cogs/other/statcord.py
@@ -7,15 +7,15 @@ class Statcord(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.StatClient(self.bot, self.bot.STATCORD_KEY)
-        self.StatsClient.start_loop()
+        self.Client = statcord.Client(self.bot, self.bot.STATCORD_KEY)
+        self.Client.start_loop()
 
     @commands.Cog.listener()
     async def on_command(self, ctx: commands.Context):
         """Event triggered when a command is executed"""
 
         self.bot.command_count += 1
-        self.StatsClient.command_run(ctx)
+        self.Client.command_run(ctx)
 
 
 def setup(bot: commands.Bot):
