0b3e9f0eb065d34be45abe3039000e1a4256a293
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-07 14:19:52 -0500

    Added ping command

--- cogs/commands/utility.py
+++ cogs/commands/utility.py
@@ -11,6 +11,17 @@ class Utility(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        """Ping command that returns the bot's latency"""
+
+        embed = discord.Embed(
+            description=f"{self.bot.e.ping} Pong! ``{round(self.bot.latency * 1000)}ms``",
+            color=self.bot.c.red
+        )
+
+        await ctx.send(embed=embed)
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Utility(bot))
