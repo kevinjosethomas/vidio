0357cd8453bae0a6ea430580c4e398724c9e6ea4
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-06 23:44:00 -0500

    Added setup database method

--- bot.py
+++ bot.py
@@ -1,15 +1,35 @@
 import os
 import dotenv
+import asyncio
+import asyncpg
 import discord
 from discord.ext import commands
 
 dotenv.load_dotenv()
 TOKEN = os.getenv("TOKEN")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASS = os.getenv("DATABASE_PASS")
 
 bot = commands.AutoShardedBot(
     command_prefix="v.",
     case_insensitive=True,
     intents=discord.Intents.default()
 )
+
+
+async def setup_database():
+    """Create a database pool connection"""
+
+    bot.database = await asyncpg.create_pool(
+        host=DATABASE_HOST,
+        database=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASS
+    )
+
+asyncio.get_event_loop().run_until_complete(setup_database())
+
 
 bot.run(TOKEN)
