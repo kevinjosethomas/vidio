369a35ae40d760ca61e7e8741ee9abc78de197a0
TrustedMercury <kevin.jt2007@gmail.com>
2021-03-08 17:11:13 -0500

    Added GuildError type

--- cogs/exceptions/__init__.py
+++ cogs/exceptions/__init__.py
@@ -5,3 +5,11 @@ class UnknownError(Exception):
     def __init__(self, reason: str = "Unknown error!"):
         self.message = reason
         super().__init__(self.message)
+
+
+class GuildError(Exception):
+    """Triggered when the error relates to guilds"""
+
+    def __init__(self, reason: str = "Unknown guild error!"):
+        self.message = reason
+        super().__init__(self.message)
